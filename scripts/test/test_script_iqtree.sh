a3ad4bc023e026a21cf050a037d075985cbae922
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2025-10-02 16:23:05 +1000

    bug fix

--- scripts/test/test_script_iqtree.sh
+++ scripts/test/test_script_iqtree.sh
@@ -36,7 +36,7 @@ for i in $(seq 1 $iter); do
                 echo "Running test for length: $length with $type"
                 if [ "$AA_or_DNA" = "AA" ]; then
                     echo "Using amino acid data"
-                    $executable_path -s alignment_${length}_aa.phy -te tree_${i}.full.treefile --prefix output_${UNIQUE_NAME}_${taxa_size}_${length}_aa_${type} -m Poisson  -blfix
+                    $executable_path -s alignment_${length}.phy -te tree_${i}.full.treefile --prefix output_${UNIQUE_NAME}_${taxa_size}_${length}_aa_${type} -m Poisson  -blfix
 
                 elif [ "$AA_or_DNA" = "DNA" ]; then
                     echo "Using DNA data"
