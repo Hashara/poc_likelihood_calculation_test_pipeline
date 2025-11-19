a8799ee3b8c733d4a49402610dd753224eb6cef9
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2025-11-19 15:11:10 +1100

    len based iqtree with omp

---
+++ scripts/test/test_script_iqtree_lenbased_omp.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+DATASET_DIR=$ARG1
+UNIQUE_NAME=$ARG2
+WD=$ARG3
+AA_or_DNA=$ARG4
+
+
+length=$ARG5
+IQTREE_THREADS=$ARG6
+executable_type=("iqtree")
+
+lengths=(100 1000 10000 100000 1000000)
+
+for length in "${lengths[@]}"; do
+  TAXA_DIR="${DATASET_DIR}/alignment_${length}"
+  echo "Processing folder: $TAXA_DIR"
+  taxa_size=$(basename "$TAXA_DIR")
+
+  echo "Current directory: $(pwd)"
+
+  cd "$TAXA_DIR" || { echo "Failed to change directory to $TAXA_DIR"; exit 1; }
+
+
+    echo "Current directory: $(pwd)"
+
+#    for length in "${lengths[@]}"; do
+        echo "Running likelihood for length: $length taxa: $taxa_size"
+
+        #loop through each executable type
+        for type in "${executable_type[@]}"; do
+            executable_path="$WD/build/iqtree_build/iqtree3"
+            echo "Using executable: $executable_path"
+
+            if [ -f "$executable_path" ]; then
+                echo "Running test for length: $length with $type"
+                if [ "$AA_or_DNA" = "AA" ]; then
+                    echo "Using amino acid data"
+                    $executable_path -s alignment_${length}.phy -te tree.full.treefile --prefix output_${UNIQUE_NAME}_${taxa_size}_${length}_aa_${type} -m Poisson  -blfix --kernel-nonrev -nt $IQTREE_THREADS
+
+                elif [ "$AA_or_DNA" = "DNA" ]; then
+                    echo "Using DNA data"
+                    $executable_path -s alignment_${length}.phy -te tree.full.treefile --prefix output_${UNIQUE_NAME}_${taxa_size}_${length}_${type} -m JC  -blfix --kernel-nonrev -nt $IQTREE_THREADS
+
+                fi
+
+                if [ $? -ne 0 ]; then
+                    echo "run failed for length: $length with $type for $taxa_size taxa"
+                    exit 1
+                fi
+            else
+                echo "Executable not found: $executable_path"
+            fi
+
+#        done
+
+    done
+
+
+
+  cd - || { echo "Failed to return to previous directory"; exit 1; }
+
+  echo "--------------------------------------"
+
+done
