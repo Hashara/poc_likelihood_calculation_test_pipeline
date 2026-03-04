c57755edfbee1953dd84fd901e02ada21c2e89a6
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2026-03-04 12:33:37 +1100

    add omp

--- scripts/qsub/iqtree/qsub_script.sh
+++ scripts/qsub/iqtree/qsub_script.sh
@@ -89,7 +89,7 @@ for r in $(seq 1 $repeat); do
       if [ "$IQTREE_OPENMP" == true ]; then
           memory=$((factor * IQTREE_THREADS * 4))
          qsub -P${PROJECT_NAME} -lwalltime=$wall_time,ncpus=$IQTREE_THREADS,mem="${memory}GB",jobfs=10GB,wd -qnormal -N test_iqtree_omp \
-                -vARG1="$DATASET_DIR",ARG2="$local_unique_name",ARG3="$WD",ARG4="$data_type",ARG5="$length",ARG6="$IQTREE_THREADS",ARG7="$IQTREE_AUTO" "$WD"/test/test_script_iqtree_omp.sh
+                -vARG1="$DATASET_DIR",ARG2="$local_unique_name",ARG3="$WD",ARG4="$data_type",ARG5="$length",ARG6="$IQTREE_THREADS",ARG7="$IQTREE_AUTO" "$WD"/test/iqtree/test_script_iqtree_omp.sh
       fi
   done
 done
